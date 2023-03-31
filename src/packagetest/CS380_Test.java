9a098818319f0e37e3059c2a551a0f5954e1dcbb
ThisIsBaoK <baok@cwu.edu>
2023-03-30 19:02:42 -0700

    Change Test to packagetest

---
+++ src/packagetest/CS380_Test.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class CS380_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The first lab CS 380.");
+		System.out.println(reverseInt(12345));
+	}
+
+	public static int reverseInt(int num) {
+		int result = 0;
+		int remainder;
+		while (num > 0) {
+			remainder = num % 10;
+			result = (result * 10) + remainder;
+			num = num / 10;
+		}
+		return result;
+	}
+}
