1075ac3187c1f5d45d6c1714960328e8be9e1a08
ThisIsBaoK <baok@cwu.edu>
2023-03-30 18:32:50 -0700

    Add java project

---
+++ src/Test/CS380_Test.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class CS380_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The first lab CS 380.");
+		
+	}
+
+}
